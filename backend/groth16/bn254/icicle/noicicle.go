b10885d102f621d31062926ac5db2a09377562b7
Jeremy Felder <jeremy.felder1@gmail.com>
2024-12-23 11:43:27 +0200

    Fix clang issue with update to icicle-gnark 3.2.2; Fix requiring icicle libs without icicle build tag

--- backend/groth16/bn254/icicle/noicicle.go
+++ backend/groth16/bn254/icicle/noicicle.go
@@ -16,3 +16,17 @@ const HasIcicle = false
 func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...backend.ProverOption) (*groth16_bn254.Proof, error) {
 	return nil, fmt.Errorf("icicle backend requested but program compiled without 'icicle' build tag")
 }
+
+type ProvingKey = groth16_bn254.ProvingKey
+
+func NewProvingKey() *ProvingKey {
+	return &ProvingKey{}
+}
+
+func Setup(r1cs *cs.R1CS, pk *ProvingKey, vk *groth16_bn254.VerifyingKey) error {
+	return groth16_bn254.Setup(r1cs, pk, vk)
+}
+
+func DummySetup(r1cs *cs.R1CS, pk *ProvingKey) error {
+	return groth16_bn254.DummySetup(r1cs, pk)
+}
